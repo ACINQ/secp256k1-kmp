07364fe59f6d18233c78e90b36ae65fe62a063ad
sstone <fabrice.drouin@acinq.fr>
2023-03-27 21:32:00 +0200

    Update Android build plugin and tools

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,13 @@ pluginManagement {
         google()
         gradlePluginPortal()
     }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace == "com.android" || requested.id.name == "kotlin-android-extensions") {
+                useModule("com.android.tools.build:gradle:7.3.1")
+            }
+        }
+    }
 }
 rootProject.name = "secp256k1-kmp"
 
