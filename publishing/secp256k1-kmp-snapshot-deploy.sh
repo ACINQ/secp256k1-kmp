7c7aabba80ee28faaae428b071e682c47cc2be7d
Fabrice Drouin <sstone@users.noreply.github.com>
2023-05-11 17:53:41 +0200

    Upgrade to Kotlin 1.8 (#81)
    * Upgrade to Kotlin 1.8
    
    * Update snapshot deployment script
    
    Kotlin 1.8 creates a new metadata jar for ios modules.

--- publishing/secp256k1-kmp-snapshot-deploy.sh
+++ publishing/secp256k1-kmp-snapshot-deploy.sh
@@ -2,52 +2,64 @@
 
 GROUP_ID=fr.acinq.secp256k1
 ARTIFACT_ID_BASE=secp256k1-kmp
-VERSION=0.9.0-SNAPSHOT
+
+if [[ -z "${VERSION}" ]]; then
+  echo "VERSION is not defined"
+  exit 1
+fi
 
 cd snapshot
 pushd .
 cd fr/acinq/secp256k1/secp256k1-kmp/$VERSION
 mvn deploy:deploy-file -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/content/repositories/snapshots/ \
-    -DpomFile=$ARTIFACT_ID_BASE-$VERSION.pom \
-    -Dfile=$ARTIFACT_ID_BASE-$VERSION.jar \
-    -Dfiles=$ARTIFACT_ID_BASE-$VERSION.module,$ARTIFACT_ID_BASE-$VERSION-kotlin-tooling-metadata.json \
-    -Dtypes=module,json \
-    -Dclassifiers=,kotlin-tooling-metadata \
-    -Dsources=$ARTIFACT_ID_BASE-$VERSION-sources.jar \
-    -Djavadoc=$ARTIFACT_ID_BASE-$VERSION-javadoc.jar
+  -DpomFile=$ARTIFACT_ID_BASE-$VERSION.pom \
+  -Dfile=$ARTIFACT_ID_BASE-$VERSION.jar \
+  -Dfiles=$ARTIFACT_ID_BASE-$VERSION.module,$ARTIFACT_ID_BASE-$VERSION-kotlin-tooling-metadata.json \
+  -Dtypes=module,json \
+  -Dclassifiers=,kotlin-tooling-metadata \
+  -Dsources=$ARTIFACT_ID_BASE-$VERSION-sources.jar \
+  -Djavadoc=$ARTIFACT_ID_BASE-$VERSION-javadoc.jar
 popd
 pushd .
-for i in iosarm64 iosx64 jni-android jni-common jni-jvm-darwin jni-jvm-extract jni-jvm-linux jni-jvm-mingw jni-jvm jvm linux
-do
-    cd fr/acinq/secp256k1/secp256k1-kmp-$i/$VERSION
-    if [ $i == iosarm64 ] || [ $i == iosx64 ] || [ $i == linux ]; then
-        mvn deploy:deploy-file -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/content/repositories/snapshots/ \
-        -DpomFile=$ARTIFACT_ID_BASE-$i-$VERSION.pom \
-        -Dfile=$ARTIFACT_ID_BASE-$i-$VERSION.klib \
-    	-Dfiles=$ARTIFACT_ID_BASE-$i-$VERSION.module,$ARTIFACT_ID_BASE-$i-$VERSION-cinterop-libsecp256k1.klib  \
-    	-Dtypes=module,klib \
-    	-Dclassifiers=,cinterop-libsecp256k1 \
-        -Dsources=$ARTIFACT_ID_BASE-$i-$VERSION-sources.jar \
-        -Djavadoc=$ARTIFACT_ID_BASE-$i-$VERSION-javadoc.jar
-    elif [ $i == jni-android ]; then
-        mvn deploy:deploy-file -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/content/repositories/snapshots/ \
-        -DpomFile=$ARTIFACT_ID_BASE-$i-$VERSION.pom \
-        -Dfile=$ARTIFACT_ID_BASE-$i-$VERSION.aar \
-    	-Dfiles=$ARTIFACT_ID_BASE-$i-$VERSION.module \
-    	-Dtypes=module \
-        -Dclassifiers= \
-    	-Dsources=$ARTIFACT_ID_BASE-$i-$VERSION-sources.jar \
-        -Djavadoc=$ARTIFACT_ID_BASE-$i-$VERSION-javadoc.jar
-    else
-        mvn deploy:deploy-file -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/content/repositories/snapshots/ \
-        -DpomFile=$ARTIFACT_ID_BASE-$i-$VERSION.pom \
-        -Dfile=$ARTIFACT_ID_BASE-$i-$VERSION.jar \
-    	-Dfiles=$ARTIFACT_ID_BASE-$i-$VERSION.module \
-    	-Dtypes=module \
-        -Dclassifiers= \
-        -Dsources=$ARTIFACT_ID_BASE-$i-$VERSION-sources.jar \
-        -Djavadoc=$ARTIFACT_ID_BASE-$i-$VERSION-javadoc.jar
-    fi    
-    popd
-    pushd .
+for i in iosarm64 iosx64 jni-android jni-common jni-jvm-darwin jni-jvm-extract jni-jvm-linux jni-jvm-mingw jni-jvm jvm linux; do
+  cd fr/acinq/secp256k1/secp256k1-kmp-$i/$VERSION
+  if [ $i == iosarm64 ] || [ $i == iosx64 ]; then
+    mvn deploy:deploy-file -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/content/repositories/snapshots/ \
+      -DpomFile=$ARTIFACT_ID_BASE-$i-$VERSION.pom \
+      -Dfile=$ARTIFACT_ID_BASE-$i-$VERSION.klib \
+      -Dfiles=$ARTIFACT_ID_BASE-$i-$VERSION-metadata.jar,$ARTIFACT_ID_BASE-$i-$VERSION.module,$ARTIFACT_ID_BASE-$i-$VERSION-cinterop-libsecp256k1.klib \
+      -Dtypes=jar,module,klib \
+      -Dclassifiers=metadata,,cinterop-libsecp256k1 \
+      -Dsources=$ARTIFACT_ID_BASE-$i-$VERSION-sources.jar \
+      -Djavadoc=$ARTIFACT_ID_BASE-$i-$VERSION-javadoc.jar
+  elif [ $i == linux ]; then
+    mvn deploy:deploy-file -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/content/repositories/snapshots/ \
+      -DpomFile=$ARTIFACT_ID_BASE-$i-$VERSION.pom \
+      -Dfile=$ARTIFACT_ID_BASE-$i-$VERSION.klib \
+      -Dfiles=$ARTIFACT_ID_BASE-$i-$VERSION.module,$ARTIFACT_ID_BASE-$i-$VERSION-cinterop-libsecp256k1.klib \
+      -Dtypes=module,klib \
+      -Dclassifiers=,cinterop-libsecp256k1 \
+      -Dsources=$ARTIFACT_ID_BASE-$i-$VERSION-sources.jar \
+      -Djavadoc=$ARTIFACT_ID_BASE-$i-$VERSION-javadoc.jar
+  elif [ $i == jni-android ]; then
+    mvn deploy:deploy-file -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/content/repositories/snapshots/ \
+      -DpomFile=$ARTIFACT_ID_BASE-$i-$VERSION.pom \
+      -Dfile=$ARTIFACT_ID_BASE-$i-$VERSION.aar \
+      -Dfiles=$ARTIFACT_ID_BASE-$i-$VERSION.module \
+      -Dtypes=module \
+      -Dclassifiers= \
+      -Dsources=$ARTIFACT_ID_BASE-$i-$VERSION-sources.jar \
+      -Djavadoc=$ARTIFACT_ID_BASE-$i-$VERSION-javadoc.jar
+  else
+    mvn deploy:deploy-file -DrepositoryId=ossrh -Durl=https://oss.sonatype.org/content/repositories/snapshots/ \
+      -DpomFile=$ARTIFACT_ID_BASE-$i-$VERSION.pom \
+      -Dfile=$ARTIFACT_ID_BASE-$i-$VERSION.jar \
+      -Dfiles=$ARTIFACT_ID_BASE-$i-$VERSION.module \
+      -Dtypes=module \
+      -Dclassifiers= \
+      -Dsources=$ARTIFACT_ID_BASE-$i-$VERSION-sources.jar \
+      -Djavadoc=$ARTIFACT_ID_BASE-$i-$VERSION-javadoc.jar
+  fi
+  popd
+  pushd .
 done
