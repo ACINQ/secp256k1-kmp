4bf5c08d54b18e976ae1bad874433553198ede5b
sstone <fabrice@acinq.fr>
2025-06-25 10:46:49 +0200

    Migrate from OSRH to Central Publisher API

--- publishing/secp256k1-kmp-staging-upload.sh
+++ publishing/secp256k1-kmp-staging-upload.sh
@@ -1,21 +1,23 @@
 #!/bin/bash -x
 #
-# first you must sign all files:
-# find release -type f -print -exec gpg -ab {} \;
+# usage:
+# VERSION=XXX ./secp256k1-kmp-staging-upload.sh create # to create bundles, which include checksums and signatures (requires access to a valid gpg key)
+# VERSION=XXX ./secp256k1-kmp-staging-upload.sh upload # to upload bundles to sonatype's staging area (requires a valid portal token)
 
 if [[ -z "${VERSION}" ]]; then
   echo "VERSION is not defined"
   exit 1
 fi
 
-if [[ -z "${OSS_USER}" ]]; then
-  echo "OSS_USER is not defined"
+if [[ -z "${CENTRAL_TOKEN_GPG_FILE}" ]]; then
+  echo "CENTRAL_TOKEN_GPG_FILE is not defined"
   exit 1
 fi
 
-read -p "Password : " -s OSS_PASSWORD
+CENTRAL_TOKEN=$(gpg --decrypt $CENTRAL_TOKEN_GPG_FILE)
 
-
+pushd .
+cd release
 for i in 	secp256k1-kmp \
 		secp256k1-kmp-iosarm64 \
 		secp256k1-kmp-iossimulatorarm64 \
@@ -33,12 +35,21 @@ for i in 	secp256k1-kmp \
 		secp256k1-kmp-macosarm64 \
 		secp256k1-kmp-macosx64
 do
-	pushd .
-	cd release/fr/acinq/secp256k1/$i/$VERSION
-	pwd
-	jar -cvf bundle.jar *
-	# use correct sonatype credentials here
-  curl -v -XPOST -u $OSS_USER:$OSS_PASSWORD --upload-file bundle.jar https://oss.sonatype.org/service/local/staging/bundle_upload
-  popd
+	DIR=fr/acinq/secp256k1/$i/$VERSION
+	echo DIR is $DIR
+	case $1 in
+	create)
+  	for file in $DIR/*
+	  do
+	    sha1sum $file | sed 's/ .*//' > $file.sha1
+	    md5sum $file | sed 's/ .*//' > $file.md5
+	    gpg -ab $file
+    done
+	  zip -r $i.zip $DIR
+	  ;;
+	upload)
+    curl --request POST --verbose --header "Authorization: Bearer ${CENTRAL_TOKEN}" --form bundle=@$i.zip https://central.sonatype.com/api/v1/publisher/upload
+    ;;
+  esac
 done
-
+popd
